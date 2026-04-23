6c46504ad41dd3860513f27d7b6bf19898851b3f
鸡肉爱土豆 <139844998+exo101@users.noreply.github.com>
2026-04-23 16:15:38 +0800

    Delete launcher directory

--- launcher/3.安装启动器依赖.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call %~dp0system\environment.bat
-echo 正在安装启动器依赖 (PyQt6, PyYAML, PyInstaller)...
-"%~dp0system\python\python.exe" -m pip install PyQt6 PyYAML pyinstaller -i https://pypi.tuna.tsinghua.edu.cn/simple
-echo.
-echo 安装完成！
-pause
